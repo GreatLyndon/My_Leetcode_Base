63d5cefa2a5179a6c39d822dd0dbc96b742863eb
Lyndon <GreatLyndon@outlook.com>
2021-10-19 19:09:11 -0700

    roman to integer

---
+++ Base/13_Roman_to_Integer.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('M', 1000);
+        map.put('D', 500);
+        map.put('C', 100);
+        map.put('L', 50);
+        map.put('X', 10);
+        map.put('V', 5);
+        map.put('I', 1);
+        int index = s.length() - 1;
+        int ans = map.get(s.charAt(index--));
+        
+        while(index >= 0) {
+            if(map.get(s.charAt(index)) >= map.get(s.charAt(index + 1)))
+                ans += map.get(s.charAt(index));
+            else
+                ans -= map.get(s.charAt(index));
+            index--;
+        }
+
+        return ans;
+    }
+}
+}
--- README.md
+++ README.md
@@ -16,3 +16,4 @@
 10.Regular Expression Matching 18/10/2021  
 11.Container With Most Water 19/10/2021  
 12.Integer to Roman 20/10/2021  
+13.Roman to Integer 20/10/2021
